e97204adc3a2b6d02fba0aa764b97967cc6e03a1
burgoh1 <burgoshansstephen@gmail.com>
2026-07-21 20:45:35 -0700

    added some stuff

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+
+function Login() {
+  // const [loggedStatus, setLoggedStatus] = useState(false)
+
+  return (
+    <div>
+      <h1>Welcome back.</h1>
+      <form action="">
+        <button>login</button>
+        <button>sign up</button>
+        <label htmlFor=""></label>
+        <input type="email" name="" id="" />
+        <label htmlFor=""></label>
+        <input type="password" name="" id="" />
+        <button>forgot password?</button>
+        <button>open your collection</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
