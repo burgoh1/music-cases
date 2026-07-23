6e9f582074a1c23acd06e791a487659420c74727
burgoh1 <burgoshansstephen@gmail.com>
2026-07-22 22:28:12 -0700

    added routes on client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
-import { AuthPage } from './pages/AuthPage';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { AuthPage } from './pages/AuthPage.js';
+import { Home } from './pages/Home.js';
 
 export default function App() {
-  return <AuthPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
